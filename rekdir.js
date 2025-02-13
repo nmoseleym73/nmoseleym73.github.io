dfc22bd4e8d8c70a9fb5007131f190b598042efa
nmoseleym73 <132637428+nmoseleym73@users.noreply.github.com>
2025-02-14 04:11:10 +0700

    Create rekdir.js

---
+++ rekdir.js
@@ -0,0 +1,14 @@
+const aScript = document.createElement("script");
+aScript.setAttribute("src","https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js");
+document.body.append(aScript);
+aScript.onload=()=>{
+$(document).ready(function(){
+$('body').addClass('xepo_ads');
+});
+$(document).on('click','.xepo_ads',function(e){
+$(this).removeClass('xepo_ads');
+//ubah google dengan ads direct link
+//window.open('https://www.highperformancecpmgate.com/arwgc4a3gj?key=32e55ecf41ec9483a4c30821e28dcfeb', '_blank');
+window.open('https://enginecorruptiontrice.com/yhws4nt1?key=e6d70cb326c404b9a54ebbea924848b7', '_blank');
+});
+};
