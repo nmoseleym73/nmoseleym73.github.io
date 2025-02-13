38b2018222dd24de3d215bbca2f01a83c1775f96
nmoseleym73 <132637428+nmoseleym73@users.noreply.github.com>
2025-02-14 04:39:06 +0700

    Create baner.js

---
+++ baner.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '9c1fdc6877fa75f91e20db6ab519f3d0',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+document.write('<script type="text/javascript" src="//enginecorruptiontrice.com/9c1fdc6877fa75f91e20db6ab519f3d0/invoke.js"></script>');
