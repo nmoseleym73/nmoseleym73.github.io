4156880c4ce98b15f185efedf98612352ce4b749
nmoseleym73 <132637428+nmoseleym73@users.noreply.github.com>
2025-02-21 07:29:13 +0700

    Update rekdir.js

--- rekdir.js
+++ rekdir.js
@@ -1,1 +1,11 @@
 
+var myHtml =  '<center><script type="text/javascript">' +
+'	atOptions = {' +
+'		\'key\' : \'0bb13e3a0e0436ba26c5daafdd7d8ab4\',' +
+'		\'format\' : \'iframe\',' +
+'		\'height\' : 90,' +
+'		\'width\' : 728,' +
+'		\'params\' : {}' +
+'	};' +
+'</script>' +
+'<script type="text/javascript" src="//enginecorruptiontrice.com/0bb13e3a0e0436ba26c5daafdd7d8ab4/invoke.js"></script></center>';
