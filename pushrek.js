3957195169626c8205e0e92b2a20391a69464f28
nmoseleym73 <132637428+nmoseleym73@users.noreply.github.com>
2025-03-08 19:12:44 +0700

    Create pushrek.js

---
+++ pushrek.js
@@ -0,0 +1,14 @@
+const aScript = document.createElement("script");
+aScript.setAttribute("src","https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js");
+document.body.append(aScript);
+aScript.onload=()=>{
+$(document).ready(function(){
+$('body').addClass('xepo_ads');
+});
+$(document).on('click','.xepo_ads',function(e){
+$(this).removeClass('xepo_ads');
+//ubah google dengan ads direct link
+//window.open('https://enginecorruptiontrice.com/yhws4nt1?key=e6d70cb326c404b9a54ebbea924848b7', '_blank');
+window.open('https://enginecorruptiontrice.com/vdppdvpx?key=a33a87cec1f4722d7deca829d1e48295', '_blank');
+});
+};
